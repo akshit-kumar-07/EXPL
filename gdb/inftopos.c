33b610d808b757baf78786d90612acdfc1e9bd99
akshit-kumar-07 <132078941+akshit-kumar-07@users.noreply.github.com>
2024-01-16 22:33:01 +0530

    Deleted gdb directory

--- gdb/inftopos.c
+++
@@ -1,8 +0,0 @@
-void infixtoprefix(FILE* fp, struct tnode* root){
-  if(root!=NULL){
-    printf("%s ",root->symbol);
-    fprintf(fp,"%s ",root->symbol);
-    infixtoprefix(fp,root->left);
-    infixtoprefix(fp,root->right);
-  }
-}
