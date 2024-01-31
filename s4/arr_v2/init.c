10ba096d469515c3a35d3bef76263ebac41c52c0
akshit-kumar-07 <132078941+akshit-kumar-07@users.noreply.github.com>
2024-01-31 16:03:25 +0530

    Extended array functionalities and typecheckings

---
+++ s4/arr_v2/init.c
@@ -0,0 +1,10 @@
+extern FILE *target_file;
+void init()
+{
+    target_file = fopen("label_code.xsm", "w");
+    fprintf(target_file, "0\n2056\n0\n0\n0\n0\n0\n0\nBRKP\n");
+    fprintf(target_file, "ADD SP,26\n");
+    fprintf(target_file, "CALL MAIN\n");
+    fprintf(target_file, "MOV R0,10\nPUSH R0\nINT 10\n");
+    fprintf(target_file, "MAIN:\n");
+}
---
+++ s4/arr_v2/parser.tab.h
@@ -0,0 +1,123 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PARSER_TAB_H_INCLUDED
+# define YY_YY_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    START = 258,                   /* START  */
+    END = 259,                     /* END  */
+    READ = 260,                    /* READ  */
+    WRITE = 261,                   /* WRITE  */
+    PLUS = 262,                    /* PLUS  */
+    MINUS = 263,                   /* MINUS  */
+    MUL = 264,                     /* MUL  */
+    DIV = 265,                     /* DIV  */
+    ASSIGN = 266,                  /* ASSIGN  */
+    NUM = 267,                     /* NUM  */
+    ID = 268,                      /* ID  */
+    IF = 269,                      /* IF  */
+    THEN = 270,                    /* THEN  */
+    ELSE = 271,                    /* ELSE  */
+    ENDIF = 272,                   /* ENDIF  */
+    WHILE = 273,                   /* WHILE  */
+    DO = 274,                      /* DO  */
+    ENDWHILE = 275,                /* ENDWHILE  */
+    BREAK = 276,                   /* BREAK  */
+    CONTINUE = 277,                /* CONTINUE  */
+    LT = 278,                      /* LT  */
+    GT = 279,                      /* GT  */
+    EQ = 280,                      /* EQ  */
+    NEQ = 281,                     /* NEQ  */
+    LE = 282,                      /* LE  */
+    GE = 283,                      /* GE  */
+    REPEAT = 284,                  /* REPEAT  */
+    UNTIL = 285,                   /* UNTIL  */
+    ENDREPEAT = 286,               /* ENDREPEAT  */
+    DOWHILE = 287,                 /* DOWHILE  */
+    ENDDOWHILE = 288,              /* ENDDOWHILE  */
+    DECL = 289,                    /* DECL  */
+    ENDDECL = 290,                 /* ENDDECL  */
+    INT = 291,                     /* INT  */
+    STR = 292,                     /* STR  */
+    STRVAL = 293,                  /* STRVAL  */
+    MOD = 294,                     /* MOD  */
+    AND = 295,                     /* AND  */
+    OR = 296                       /* OR  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 23 "parser.y"
+
+    struct tnode *node;
+
+#line 109 "parser.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
